63ab16d63f17ab6ee74c9722a61d4d82979529c2
c-s-cook <christopherstephencook@gmail.com>
2022-10-12 17:11:10 -0500

    connecting Django views to RestAPIs

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 # from .restapis import related methods
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -95,15 +96,24 @@ def registration_request(request):
 
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        # OG >>> return render(request, 'djangoapp/index.html', context)
+        url = "https://us-south.functions.appdomain.cloud/api/v1/web/89f97b1c-5e62-40b3-b586-9642b92e5de0/dealership-package/get-dealership.json"
+        dealerships = get_dealers_from_cf(url)
+        dealer_names = ' <p> '.join([dealer.short_name for dealer in dealerships])
+        return HttpResponse(dealer_names)
+
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
-# ...
+
+
+
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
